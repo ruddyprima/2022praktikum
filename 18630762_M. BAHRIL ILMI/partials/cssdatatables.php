890f72a1bd46b3317821ac4498f866347bc70fbd
bahrilio <mbahrililmi2@gmail.com>
2022-01-24 23:03:46 +0700

    pertemuan 1

---
+++ 18630762_M. BAHRIL ILMI/partials/cssdatatables.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.boostrap4.min.css">
+<link rel="stylesheet" href="plugins/datatables-buttons/css/buttons.boostrap4.min.css">
