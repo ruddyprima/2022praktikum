f68c73c0cb5ede5866f75fca143874119af4f1ca
Muhammad Dhafin Harish <DHaris454@gmail.com>
2022-01-27 15:20:58 +0800

    Menampilkan halaman read lokasi

--- 18631034_MUHAMMAD DHAFIN HARISH/routes.php
+++ 18631034_MUHAMMAD DHAFIN HARISH/routes.php
@@ -2,13 +2,16 @@
 if (isset($_GET['page'])) {
     $page = $_GET['page'];
     switch ($page) {
-        case '':
-        case 'home' :
-            file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
+        case '';
+        case 'home':
+            file_exists('pages/home.php') ? include 'pages/home.php' : include 'pages/404.php';
             break;
-        default:
-            include "pages/404.php";
+        case 'lokasiread':
+            file_exists('pages/admin/lokasiread.php') ? include 'pages/admin/lokasiread.php' : include 'pages/404.php';
+            break;
+        default;
+            include 'pages/404.php';
     }
 } else {
-    include "pages/home.php";
+    include 'pages/home.php';
 }
