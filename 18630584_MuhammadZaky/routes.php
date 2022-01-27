8083224abe0812b6f53c2e3b7cdb4654cde561b1
zakyyyy <mzaky2678@gmail.com>
2022-01-26 22:00:12 -0800

    Membuat Route

--- 18630584_MuhammadZaky/routes.php
+++ 18630584_MuhammadZaky/routes.php
@@ -3,7 +3,7 @@ if (isset($_GET['page'])) {
     $page = $_GET['page'];
     switch ($page) {
         case '':
-        case 'home' :
+        case 'home':
             file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
             break;
         default:
