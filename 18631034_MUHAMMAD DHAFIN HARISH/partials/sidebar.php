1f45f56e502a7c5296a77ae1c60bfadea15bdfce
Muhammad Dhafin Harish <DHaris454@gmail.com>
2022-01-27 13:37:58 +0800

    update sidebar.php

--- 18631034_MUHAMMAD DHAFIN HARISH/partials/sidebar.php
+++ 18631034_MUHAMMAD DHAFIN HARISH/partials/sidebar.php
@@ -7,14 +7,15 @@ img-circle elevation-3" style="opacity: .8">
   <div class="sidebar">
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation2" alt="User Image">
+        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-
+2" alt="User Image">
       </div>
       <div class="info">
         <a href="#" class="d-block">Admin</a>
       </div>
     </div>
     <nav class="mt-2">
-      <ul class="nav nav-pills nav-sidebar flex-column" datawidget="treeview" role="menu" data-accordion="false">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
           <a href="?page=home" class="nav-link active">
             <i class="nav-icon fas fa-home"></i>
