1353dee6d7f88a62538128b42670088623365ed5
irfani12 <96363883+irfani12@users.noreply.github.com>
2022-01-28 10:08:22 +0800

    slidebar

--- 00NPM00_Fathul_Hafidh/partials/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-        <b>Version</b> 0.1.0
-    </div>
-    <strong>Copyright &copy; 2022 <a href="https://mirzayogy.github.io">Praktikum Web</a>.</strong> All rights reserved.
-</footer>
