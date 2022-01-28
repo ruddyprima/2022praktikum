e318e87538bc41166ee038a94b3fe6a50c23f06d
c-syamsulhadi-si <c.syamsulhadi.si@gmail.com>
2022-01-29 04:24:46 +0800

    Membuat folder partials dengan file sidebarmenu(berisikan menu)

---
+++ 18710098_Syamsul_Hadi/partials/sidebar.php
@@ -0,0 +1,34 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+            <a href="?page=home" class="nav-link">
+                <i class="nav-icon fas fa-th"></i>
+                <p>Starter Page</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="?page=lokasi" class="nav-link">
+                <i class="nav-icon fas fa-map-marker"></i>
+                <p>Lokasi</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="?page=bagian" class="nav-link">
+                <i class="nav-icon fas fa-sitemap"></i>
+                <p>Bagian</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="?page=jabatan" class="nav-link">
+                <i class="nav-icon fas fa-eject"></i>
+                <p>Jabatan</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="?page=karyawan" class="nav-link">
+                <i class="nav-icon fas fa-user"></i>
+                <p>Karyawan</p>
+            </a>
+        </li>
+    </ul>
+</nav>
