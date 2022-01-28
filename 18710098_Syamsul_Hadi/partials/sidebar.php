244d0e0f78c3b18bf4b58ae5b3ac33d1d9dccad1
c-syamsulhadi-si <c.syamsulhadi.si@gmail.com>
2022-01-29 05:05:03 +0800

    Membuat fitur active menu

--- 18710098_Syamsul_Hadi/partials/sidebar.php
+++ 18710098_Syamsul_Hadi/partials/sidebar.php
@@ -1,3 +1,41 @@
+<?php
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+    switch ($page) {
+        case 'lokasi':
+            $active_menu_lokasi = "active";
+            $active_menu_jabatan = "";
+            $active_menu_bagian = "";
+            $active_menu_karyawan = "";
+            break;
+        case 'jabatan':
+            $active_menu_lokasi = "";
+            $active_menu_jabatan = "active";
+            $active_menu_bagian = "";
+            $active_menu_karyawan = "";
+            break;
+        case 'bagian':
+            $active_menu_lokasi = "";
+            $active_menu_jabatan = "";
+            $active_menu_bagian = "active";
+            $active_menu_karyawan = "";
+            break;
+        case 'karyawan':
+            $active_menu_lokasi = "";
+            $active_menu_jabatan = "";
+            $active_menu_bagian = "";
+            $active_menu_karyawan = "active";
+            break;
+        default:
+            $active_menu_lokasi = "";
+            $active_menu_jabatan = "";
+            $active_menu_bagian = "";
+            $active_menu_karyawan = "";
+            break;
+    }
+};
+?>
+
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
@@ -7,25 +45,25 @@
             </a>
         </li>
         <li class="nav-item">
-            <a href="?page=lokasi" class="nav-link">
+            <a href="?page=lokasi" class="nav-link <?= $active_menu_lokasi; ?>">
                 <i class="nav-icon fas fa-map-marker"></i>
                 <p>Lokasi</p>
             </a>
         </li>
         <li class="nav-item">
-            <a href="?page=bagian" class="nav-link">
-                <i class="nav-icon fas fa-sitemap"></i>
-                <p>Bagian</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="?page=jabatan" class="nav-link">
+            <a href="?page=jabatan" class="nav-link <?= $active_menu_jabatan; ?>">
                 <i class="nav-icon fas fa-eject"></i>
                 <p>Jabatan</p>
             </a>
         </li>
         <li class="nav-item">
-            <a href="?page=karyawan" class="nav-link">
+            <a href="?page=bagian" class="nav-link <?= $active_menu_bagian; ?>">
+                <i class="nav-icon fas fa-sitemap"></i>
+                <p>Bagian</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="?page=karyawan" class="nav-link <?= $active_menu_karyawan; ?>">
                 <i class="nav-icon fas fa-user"></i>
                 <p>Karyawan</p>
             </a>
