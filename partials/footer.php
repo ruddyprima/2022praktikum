115061ad3323f226d8a7fce1186f1d0d55743746
ruddyprima <primahendra70@gmail.com>
2022-01-30 08:21:27 +0700

    praktikum 1

---
+++ partials/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+  <div class="float-right d-none d-sm-block">
+    <b>Version</b> 0.1.0
+  </div>
+  <strong>Copyright &copy; <?= date('Y'); ?> <a href="https://adminlte.io">Praktikum Web</a>.</strong> All rights reserved.
+</footer>
